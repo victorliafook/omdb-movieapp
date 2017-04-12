1c27439a5aea7d2a06d5a9102ade6de4e70c8ef6
Victor Lia Fook <victorliafook@gmail.com>
2017-04-12 08:45:47 +0000

    some changes and better practices in the service.

--- dist/js/app.js
+++ dist/js/app.js
@@ -121,33 +121,21 @@ angular.module("omdbApp")
     }])
     .service('dataService', function(){
         this.urlParams = {imdbId: 'i', title: 's', type: 'type', year: 'y', plot: 'plot', page: 'page'};
-        this.get = function(param){
-            return (typeof this.urlParams[param] !== 'undefined') ? this.urlParams[param] : null;
+        this.getParam = function(param){
+            return (this.urlParams[param] !== undefined) ? this.urlParams[param] : null;
         };
         
         this.getQueryObj = function(arr){
             var len = arr.length;
             var retObj = {};
-            if(typeof arr === 'undefined' || len <= 0) return retObj;
+            if(undefined || len <= 0) return retObj;
             
-            var objStr = "{";
             for(var i = 0; i < len; i++){
                 if(arr[i].value == "") continue;
-                objStr += "\"" + this.get(arr[i].key) + "\": \"" + arr[i].value + "\",";
+                var paramName = this.getParam(arr[i].key);
+                retObj[paramName] = arr[i].value;
             }
-            
-            if(objStr.length > 1){
-                objStr = objStr.substring(0, objStr.length - 1) + "}";
-            }else{
-                objStr = '{}';
-            }
-            
-            try{
-                retObj = JSON.parse(objStr);
-            }catch(err){
-                console.log(err);
-                retObj = {};
-            }
+           
             return retObj;
         };
         
